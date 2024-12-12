a57ae6f6537919f8631d5fbb589e686de3869173
Sujal <palsujal707@gmail.com>
2024-12-13 03:44:14 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,5 @@ app.use(bodyParser.json());
 //routes
 app.use('/api/images', imageRoutes);
 
-const PORT = process.env.PORT ||4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
